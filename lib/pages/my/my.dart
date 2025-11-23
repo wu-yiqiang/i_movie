c917e6fd0485407721cf868d7aa1b4a571f25649
wu-yiqiang <wu_yiqiang@outlook.com>
2025-11-23 17:51:01 +0800

    添加我的页面

--- lib/pages/my/my.dart
+++ lib/pages/my/my.dart
@@ -11,7 +11,223 @@ class _MyPage extends State<MyPage> {
   @override
   Widget build(BuildContext context) {
     return SafeArea(
-      child: Scaffold(body: Container(child: Text("我的"), padding: EdgeInsets.all(padding),)),
+      child: Scaffold(
+        appBar: AppBar(
+          actions: [
+            IconButton(
+              onPressed: () {},
+              icon: Icon(Icons.qr_code_scanner_outlined, size: 24),
+            ),
+            IconButton(
+              onPressed: () {},
+              icon: Icon(Icons.qr_code_outlined, size: 24),
+            ),
+            IconButton(
+              onPressed: () {},
+              icon: Icon(Icons.dark_mode_outlined, size: 24),
+            ),
+          ],
+        ),
+        body: Container(
+          child: Column(
+            spacing: 20,
+            children: [
+              Row(
+                children: [
+                  Expanded(
+                    child: Flex(
+                      direction: Axis.horizontal,
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Row(
+                          spacing: 10,
+                          children: [
+                            ClipRRect(
+                              borderRadius: BorderRadius.circular(35),
+                              child: Image.asset(
+                                "images/user.jpg",
+                                fit: BoxFit.fill,
+                                width: 70,
+                              ),
+                            ),
+                            Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Text("Sutter", style: TextStyle(fontSize: 20)),
+                                Text(
+                                  "这个人什么也没有留下",
+                                  style: TextStyle(fontSize: 14),
+                                ),
+                              ],
+                            ),
+                          ],
+                        ),
+                        Icon(
+                          Icons.keyboard_arrow_right_outlined,
+                          size: 24,
+                          color: Colors.grey,
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+              Row(
+                children: [
+                  Expanded(child: Column(children: [Text('0'), Text("动态")])),
+                  Expanded(child: Column(children: [Text('0'), Text("关注")])),
+                  Expanded(child: Column(children: [Text('0'), Text("粉丝")])),
+                ],
+              ),
+              Flex(
+                direction: Axis.horizontal,
+                children: [
+                  Expanded(
+                    flex: 1,
+                    child: IconButton(
+                      padding: EdgeInsets.zero,
+                      alignment: Alignment.center,
+                      icon: Flex(
+                        spacing: 4,
+                        direction: Axis.vertical,
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Icon(Icons.history, size: 24, color: Colors.blue),
+                          SizedBox(
+                            child: Text(
+                              '历史记录',
+                              style: TextStyle(
+                                fontSize: 13,
+                                color: Colors.grey,
+                                fontWeight: FontWeight.w500,
+                              ),
+                              overflow: TextOverflow.ellipsis,
+                            ),
+                          ),
+                        ],
+                      ),
+                      onPressed: () {
+                        // eventBus.emit(Events.NAVIGATE.name, v['path']);
+                      },
+                    ),
+                  ),
+                  Expanded(
+                    flex: 1,
+                    child: IconButton(
+                      padding: EdgeInsets.zero,
+                      alignment: Alignment.center,
+                      icon: Flex(
+                        spacing: 4,
+                        direction: Axis.vertical,
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Icon(
+                            Icons.star_outline,
+                            size: 24,
+                            color: Colors.blue,
+                          ),
+                          SizedBox(
+                            child: Text(
+                              '我的收藏',
+                              style: TextStyle(
+                                fontSize: 13,
+                                color: Colors.grey,
+                                fontWeight: FontWeight.w500,
+                              ),
+                              overflow: TextOverflow.ellipsis,
+                            ),
+                          ),
+                        ],
+                      ),
+                      onPressed: () {
+                        // eventBus.emit(Events.NAVIGATE.name, v['path']);
+                      },
+                    ),
+                  ),
+                ],
+              ),
+              Row(
+                children: [
+                  Expanded(
+                    flex: 1,
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Container(
+                          child: Text("更多服务"),
+                          margin: EdgeInsets.symmetric(vertical: 10),
+                        ),
+                        Padding(
+                          padding: EdgeInsets.only(left: 20),
+                          child: Column(
+                            spacing: 20,
+                            children: [
+                              Flex(
+                                direction: Axis.horizontal,
+                                mainAxisAlignment:
+                                    MainAxisAlignment.spaceBetween,
+                                children: [
+                                  Row(
+                                    spacing: 6,
+                                    children: [
+                                      Icon(
+                                        Icons.settings,
+                                        size: 22,
+                                        color: primary,
+                                      ),
+                                      Text(
+                                        "设置",
+                                        style: TextStyle(fontSize: 16),
+                                      ),
+                                    ],
+                                  ),
+                                  Icon(
+                                    Icons.keyboard_arrow_right_outlined,
+                                    size: 24,
+                                    color: Colors.grey,
+                                  ),
+                                ],
+                              ),
+                              Flex(
+                                direction: Axis.horizontal,
+                                mainAxisAlignment:
+                                    MainAxisAlignment.spaceBetween,
+                                children: [
+                                  Row(
+                                    spacing: 6,
+                                    children: [
+                                      Icon(
+                                        Icons.smart_toy,
+                                        size: 22,
+                                        color: primary,
+                                      ),
+                                      Text(
+                                        "联系客服",
+                                        style: TextStyle(fontSize: 16),
+                                      ),
+                                    ],
+                                  ),
+                                  Icon(
+                                    Icons.keyboard_arrow_right_outlined,
+                                    size: 24,
+                                    color: Colors.grey,
+                                  ),
+                                ],
+                              ),
+                            ],
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
+          padding: EdgeInsets.all(padding),
+        ),
+      ),
     );
   }
 }
--- lib/pages/tabs/recommend/recommend.dart
+++ lib/pages/tabs/recommend/recommend.dart
@@ -3,7 +3,7 @@ import 'package:i_movie/common/const.dart';
 
 class RecommendPage extends StatefulWidget {
   const RecommendPage({super.key});
- 
+
   @override
   _RecommendPage createState() => _RecommendPage();
 }
@@ -13,35 +13,35 @@ class _RecommendPage extends State<RecommendPage> {
     {
       'id': 12,
       'label': '如何将windows 11任务栏订制成dock栏风格',
-      'account': '麦嘎登Linux',
+      'account': '计算机教育',
       'image':
           "https://gips1.baidu.com/it/u=1025173963,4205445645&fm=3028&app=3028&f=JPEG&fmt=auto?w=3200&h=3200",
     },
     {
       'id': 13,
       'label': '通用汽车停用中国供应商零部件',
-      'account': '联合早报',
+      'account': '新闻早报',
       'image':
           "https://gips1.baidu.com/it/u=1025173963,4205445645&fm=3028&app=3028&f=JPEG&fmt=auto?w=3200&h=3200",
     },
     {
       'id': 14,
       'label': 'async await底层是如何实现的',
-      'account': '渡一教育',
+      'account': '计算机教育',
       'image':
           "https://gips1.baidu.com/it/u=1025173963,4205445645&fm=3028&app=3028&f=JPEG&fmt=auto?w=3200&h=3200",
     },
     {
       'id': 15,
       'label': '你真的理解vue的生命周期吗？',
-      'account': '渡一教育',
+      'account': '计算机教育',
       'image':
           "https://gips1.baidu.com/it/u=1025173963,4205445645&fm=3028&app=3028&f=JPEG&fmt=auto?w=3200&h=3200",
     },
     {
       'id': 16,
       'label': 'Go语言从入门到精通',
-      'account': 'Google中国',
+      'account': 'go语言中文社区',
       'image':
           "https://gips1.baidu.com/it/u=1025173963,4205445645&fm=3028&app=3028&f=JPEG&fmt=auto?w=3200&h=3200",
     },
@@ -55,90 +55,116 @@ class _RecommendPage extends State<RecommendPage> {
     {
       'id': 18,
       'label': '个人所得税率大涨27.26%',
-      'account': '王局拍案',
+      'account': '新闻早报',
       'image':
           "https://gips1.baidu.com/it/u=1025173963,4205445645&fm=3028&app=3028&f=JPEG&fmt=auto?w=3200&h=3200",
     },
     {
       'id': 19,
-      'label': 'sadsadasd',
-      'account': '麦嘎登Linux',
+      'label': '网络是如何连接的',
+      'account': '网络社区',
       'image':
           "https://gips1.baidu.com/it/u=1025173963,4205445645&fm=3028&app=3028&f=JPEG&fmt=auto?w=3200&h=3200",
     },
     {
       'id': 20,
-      'label': 'sadsadasd',
-      'account': '麦嘎登Linux',
+      'label': '如何计算子网掩码',
+      'account': 'linux教育',
       'image':
           "https://gips1.baidu.com/it/u=1025173963,4205445645&fm=3028&app=3028&f=JPEG&fmt=auto?w=3200&h=3200",
     },
   ];
+
+  Future<void> _onRefresh() async {
+    await Future.delayed(Duration(seconds: 2));
+    // setState(() {
+    //   items.addAll(
+    //     List.generate(20, (i) => 'New item ${i + items.length + 1}'),
+    //   );
+    // });
+  }
+
+  handleFresh() {
+    Future<void> _onRefresh() async {
+      await Future.delayed(Duration(seconds: 2)); // 模拟网络请求
+      // 更新数据
+      setState(() {
+        this.items.addAll(
+          List.generate(20, (i) => 'New item ${i + items.length + 1}'),
+        );
+      });
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Container(
         padding: EdgeInsets.all(padding),
-        child: GridView.builder(
-          shrinkWrap: true,
-          itemCount: 9,
-          gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-            crossAxisSpacing: 10,
-            crossAxisCount: 2,
-            mainAxisSpacing: 10,
-            childAspectRatio: 0.77,
-          ),
-          itemBuilder: (context, index) {
-            return Container(
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                crossAxisAlignment: CrossAxisAlignment.start,
-                spacing: 4,
-                children: [
-                  ClipRRect(
-                    borderRadius: BorderRadius.circular(6),
-                    child: Image.network(
-                      items[index]['image']!,
-                      fit: BoxFit.fitWidth,
-                      alignment: Alignment.center,
-                    ),
-                  ),
-                  Container(
-                    padding: EdgeInsets.all(4),
-                    decoration: BoxDecoration(
+        child: RefreshIndicator(
+          color: primary,
+          onRefresh: _onRefresh,
+          child: GridView.builder(
+            shrinkWrap: true,
+            itemCount: 9,
+            gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+              crossAxisSpacing: 10,
+              crossAxisCount: 2,
+              mainAxisSpacing: 10,
+              childAspectRatio: 0.77,
+            ),
+            itemBuilder: (context, index) {
+              return Container(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  spacing: 4,
+                  children: [
+                    ClipRRect(
                       borderRadius: BorderRadius.circular(6),
-                      // border: BoxBorder.all(color: primary),
+                      child: Image.network(
+                        items[index]['image']!,
+                        fit: BoxFit.fitWidth,
+                        alignment: Alignment.center,
+                      ),
                     ),
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text(
-                          items[index]['label'],
-                          overflow: TextOverflow.ellipsis,
-                          style: TextStyle(fontSize: 16),
-                        ),
-                        Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          children: [
-                            Text(
-                              items[index]['account'],
-                              style: TextStyle(fontSize: 14),
-                            ),
-                            Icon(Icons.more_vert, size: 16),
-                          ],
-                        ),
-                      ],
+                    Container(
+                      padding: EdgeInsets.all(4),
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(6),
+                        // border: BoxBorder.all(color: primary),
+                      ),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            items[index]['label'],
+                            overflow: TextOverflow.ellipsis,
+                            style: TextStyle(fontSize: 16),
+                          ),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              Text(
+                                items[index]['account'],
+                                style: TextStyle(fontSize: 14),
+                              ),
+                              Icon(Icons.more_vert, size: 16),
+                            ],
+                          ),
+                        ],
+                      ),
                     ),
-                  ),
-                ],
-              ),
-              // padding: EdgeInsets.all(6),
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(6),
-                border: BoxBorder.all(color: primary),
-              ),
-            );
-          },
+                  ],
+                ),
+                // padding: EdgeInsets.all(6),
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(6),
+                  border: BoxBorder.all(color: primary),
+                ),
+              );
+            },
+          ),
         ),
       ),
     );
